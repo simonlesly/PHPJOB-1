429039c49d9fa4d2a0d01e2a960d9ce698427598
simonl <121858461+simonlesly@users.noreply.github.com>
2023-01-27 20:43:03 +0100

    Update traitement.php

--- PARTIEL/traitement.php
+++ PARTIEL/traitement.php
@@ -2,10 +2,9 @@
 
 require "ressources.php";
 
-$developpeur = array(new Developpeur(550,"Le Flock", "Louis-Marie", "lmleflock@test.net", "0606060606",2500, "Java"), git status -s --untracked-files=no new Developpeur(650,"Poirie", "Ethan", "ach@test.net", "0707070707",2750, "C++")) ;
- 
-$manager = array() ;
-
+$developpeur = array(new Developpeur(550,"Le Flock", "Louis-Marie","lmleflock@test.net", "0606060606",2500, "Java"),
+new Developpeur(650,"Chellal", "Amine","ach@test.net", "0707070707",2750, "C++"),new Developpeur(151,"Katsiev", "Amir","amir@test.net", "0606060606",2750, "Kotlin")) ;
+$manager = array(new Manager(500,"RIPAULT", "Alexis", "alexis@manager.net","0606060606",4000, "DSI")) ;
 
 foreach ($developpeur as $value) {
 	echo $value->afficher();
